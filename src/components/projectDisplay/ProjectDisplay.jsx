b573f5c7327c535da9d2697977630e935ba2f2a6
malcomcrab <malcomcrabesq@outlook.com>
2025-03-24 15:01:03 +0000

    Style: Adds CSS module and basic styling for ProjectDisplay component.

--- src/components/projectDisplay/ProjectDisplay.jsx
+++ src/components/projectDisplay/ProjectDisplay.jsx
@@ -1,11 +1,23 @@
-
+import styles from "./projectDisplay.module.css"
 function ProjectDisplay(){
 
 
     return(
-        <>
-            <p>projects</p>
-        </>
+        <div className={styles.projectDisplayContainer}>
+
+            <div className={styles.headingContainer}>
+                <h1 className={styles.mainHeading}>Projects</h1>
+            </div>
+
+            <div className={styles.projectsContainer}>
+                <div>project 1</div>
+                <div>project 2</div>
+                <div>project 3</div>
+                <div>project 4</div>
+                <div>project 5</div>
+                <div>project 6</div>
+            </div>
+        </div>
     )
 
 }
