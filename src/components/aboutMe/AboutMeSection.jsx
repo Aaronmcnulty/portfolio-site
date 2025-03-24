147457ffd9d0ab96ca6e1abca6a2cb1fdb780a42
malcomcrab <malcomcrabesq@outlook.com>
2025-03-24 15:26:42 +0000

    Style: Adds CSS module and styling to AboutMeSection component

--- src/components/aboutMe/AboutMeSection.jsx
+++ src/components/aboutMe/AboutMeSection.jsx
@@ -1,12 +1,25 @@
+import styles from "./aboutMeSection.module.css"
 
 function AboutMeSection(){
 
     return(
-        <div>
-            <p>about me.</p>
+        <div className={styles.AboutMeContainer}>
+           
+           <div className={styles.HeadingContainer}>
+                <h1 className={styles.mainHeading}>
+                    About
+                </h1>
+           </div>
+
+            <div className={styles.aboutTextContainer}>
+                <p> Blah</p>
+                <p> blah</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Nullam cursus diam turpis, nec pellentesque tortor convallis non.
+                    Praesent fringilla non diam non tempus. </p>
+            </div>
         </div>
     )
-
 }
 
 export default AboutMeSection
--- src/components/navigation/navbar/NavBar.jsx
+++ src/components/navigation/navbar/NavBar.jsx
@@ -6,8 +6,8 @@ function NavBar(){
     return(
         <div className={styles.navBarContainer}>
             <Link className={styles.navBarLink} to="/">Home</Link>
-            <Link className={styles.navBarLink} to="projects">Projects</Link>
             <Link className={styles.navBarLink} to="about">About</Link>
+            <Link className={styles.navBarLink} to="projects">Projects</Link>
             <Link className={styles.navBarLink} to="contact">Contact</Link>
         </div>
     )
